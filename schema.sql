14929c0907fd6d92b2ddaacbe26515c666e8ecd1
rnmona99 <swxrwktbh7@privaterelay.appleid.com>
2026-01-07 05:39:59 +0300

    Create initial database schema with tables

---
+++ schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE members (...);
+CREATE TABLE providers (...);
+CREATE TABLE procedures (...);
+CREATE TABLE claims (...);
+CREATE TABLE claim_lines (...);
+CREATE TABLE preauth_requests (...);
